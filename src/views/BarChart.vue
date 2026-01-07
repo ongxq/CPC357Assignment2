5fea21ccf0431a240285d317e8c80b35fc22cc5b
ONG XIU QIN <xqwow1024@gmail.com>
2026-01-07 20:43:40 +0800

    Dashboard Visualization CPC357

---
+++ src/views/BarChart.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="chart-wrapper">
+    <Bar :data="chartData" :options="chartOptions" />
+  </div>
+</template>
+
+<script setup>
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+import { Bar } from "vue-chartjs";
+
+ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
+
+defineProps({
+  chartData: Object,
+  chartOptions: Object,
+});
+</script>
---
+++ src/views/LineChart.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { defineProps, watch, onMounted, ref } from "vue";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  PointElement,
+  LinearScale,
+  CategoryScale,
+  TimeScale,
+} from "chart.js";
+import { Line } from "vue-chartjs";
+import "chartjs-adapter-date-fns";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  PointElement,
+  LinearScale,
+  CategoryScale,
+  TimeScale
+);
+
+const props = defineProps({
+  chartData: Object,
+  chartOptions: Object,
+});
+</script>
+
+<template>
+  <Line :data="chartData" :options="chartOptions" />
+</template>
